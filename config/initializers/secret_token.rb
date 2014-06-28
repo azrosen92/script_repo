83a6a9643511408b02d58b0c9746135cdf5924d8
Aaron Rosen <azrosen92@gmail.com>
2014-06-28 08:54:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScriptRepo::Application.config.secret_token = '0545b1712ab01ee006570476d4de9293d0c9ed9c0bce714b2a261df8f533b2cbe42d86cbc8117c837968b4538478f68b5dc6bb82222c25e233726a8f16bce17a'
